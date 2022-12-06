a22fb339424f8e67f16658afa05969dd0664cd50
bocahkoding <kodingbocah@gmail.com>
2022-12-06 21:42:58 +0700

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['index.html'],
+  darkMode: 'class',
   theme: {
     container: {
       center: true,
-      padding: '8px',
+      padding: '4px',
     },
     extend: {
       colors: {
