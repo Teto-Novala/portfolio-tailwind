b3da4ff6b7ba4a48e839e056023af5c7278e437e
bocahkoding <kodingbocah@gmail.com>
2022-12-11 22:21:37 +0700

    update darkmode

--- dist/js/script.js
+++ dist/js/script.js
@@ -41,18 +41,20 @@ const darkToggle = document.querySelector('#dark-toggle');
 const html = document.querySelector('html');
 
 darkToggle.addEventListener('click', function() {
-    if(darkToggle.checked) {
+    if(darkToggle.checked){
         html.classList.add('dark');
-        localStorage.Theme = 'dark';
+        localStorage.theme = 'dark';
     } else {
         html.classList.remove('dark');
-        localStorage.Theme = 'light';
+        localStorage.theme = 'light';
     }
-});
+})
+
+
 
 // pindahkan posisi toggle sesuai mode
 if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
     darkToggle.checked = true;
-  } else {
+} else {
     darkToggle.checked = false;
   }
